000883ffffbdd332482fbbb5e90c7e2ee7839ad9
Jaren <kaylajaren@gmail.com>
2025-04-24 11:49:18 +0800

    feat(frontend): Added support for autoloading env variables + create sample landing page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+const { config } = require('dotenv');
+const path = require('path');
+
+config({ path: path.resolve(__dirname, '../.env') });
 
 const nextConfig: NextConfig = {
   /* config options here */
