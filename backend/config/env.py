5c00029b268f79ca3d8d1717f59296be365ada67
Jaren <kaylajaren@gmail.com>
2025-07-09 15:32:05 +0800

    chore(backend): update env config file

--- backend/config/env.py
+++ backend/config/env.py
@@ -9,11 +9,19 @@ class App_Config(BaseSettings):
     # Backend
     NEXT_PUBLIC_BACKEND_URL: str
     DATABASE_URL: str
+    DATABASE_URL_PGVECTOR: str
     OPEN_AI_API_KEY: str
-    UPLOAD_DIR: str
     ELEVEN_LAB_API_KEY: str
     ASSEMBLY_AI_API_KEY: str
     IMAGEMAGICK_BINARY: str
+
+    # Static file directories
+    SUBTITLES_DIR: str
+    OUTPUT_DIR: str
+    TEMP_DIR: str
+    VIDEOS_DIR: str
+    UPLOAD_DIR: str
+    
     # Config for loading environment variables
     model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
 
